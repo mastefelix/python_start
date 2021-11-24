05c2cd08fcec3637c891800eed3127a1872d1393
mastefelix <alexandr.kurganskii@yandex.ru>
2021-11-24 09:31:53 +0300

    4.5

--- lesson4/4.4.py
+++ lesson4/4.4.py
@@ -1,6 +1,6 @@
 from random import randint
 
 my_list = [randint(0, 10) for _ in range(0, 10)]
-un_list = [i for i in my_list if my_list.count(i) == 1]
+un_list = filter(lambda i: (my_list.count(i) == 1), my_list)
 print(my_list)
-print(un_list)
+print(list(un_list))
