b466660571d8c83d5eba72606f9ac973968a5b8a
mastefelix <alexandr.kurganskii@yandex.ru>
2021-12-01 00:08:11 +0300

    6.1 add \r

--- lesson6/6.1.py
+++ lesson6/6.1.py
@@ -6,13 +6,13 @@ class TrafficLight:
 
     def running(self):
         while True:
-            print(f'\033[31m{self.__color[0]}')
+            print(f'\r\033[31m{self.__color[0]}', end='')
             sleep(7)
-            print(f'\033[33m{self.__color[1]}')
+            print(f'\r\033[33m{self.__color[1]}', end='')
             sleep(2)
-            print(f'\033[32m{self.__color[2]}')
+            print(f'\r\033[32m{self.__color[2]}', end='')
             sleep(10)
-            print(f'\033[33m{self.__color[1]}')
+            print(f'\r\033[33m{self.__color[1]}', end='')
             sleep(2)
 
 
